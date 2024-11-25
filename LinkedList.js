b60d61ac587dd667b4bbcf4222970c3d8eacbf6c
Danthi-Aishwarya <aishwaryadanthi91@gmail.com>
2024-11-25 17:33:23 +0530

    LINKED LIST

---
+++ LinkedList.js
@@ -0,0 +1,63 @@
+import { Node } from "./node.js";
+export class linkedlist {
+  constructor() {
+    this.linkhead = null;
+  }
+  prepend(newnode) {
+    let temp = this.linkhead;
+    if (this.linkhead == null) {
+      this.linkhead = new Node(newnode);
+    } else {
+      this.linkhead = new Node(newnode);
+      this.linkhead.next = temp;
+    }
+  }
+  append(newnode) {
+    if (this.linkhead == null) {
+      this.linkhead = new Node(newnode);
+    } else {
+      let temp = this.linkhead;
+      while (temp.next !== null) {
+        temp = temp.next;
+      }
+      temp.next = new Node(newnode);
+    }
+  }
+  isstring() {
+    let temp = this.linkhead;
+    let store = "";
+    while (temp !== null) {
+      store += `(${temp.value})=>`;
+      temp = temp.next;
+    }
+    console.log(store);
+  }
+  size() {
+    let count = 0;
+    let temp = this.linkhead;
+    while (temp !== null) {
+      count += 1;
+      temp = temp.next;
+    }
+    console.log(count);
+  }
+  head() {
+    console.log(this.linkhead.value);
+  }
+  tail() {
+    let temp = this.linkhead;
+    while (temp.next !== null) {
+      temp = temp.next;
+    }
+    console.log(temp.value);
+  }
+  at(index) {
+    let temp = this.linkhead;
+    let c = 0;
+    while (temp.value !== index) {
+      temp = temp.next;
+      c = c + 1;
+    }
+    console.log(c);
+  }
+}
